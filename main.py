6afd988ad9894b83b795aa678ed776efd05d9f64
KaiNeon <neon030800@gmail.com>
2022-06-16 05:59:05 +0500

    Добавил базу с реальными фактами и решениями. Переработал вывод информации о решении.

--- main.py
+++ main.py
@@ -6,16 +6,32 @@ from db_module import DataBase
 logging.basicConfig(level=logging.WARNING)
 
 
+def line_format(line, words):
+    buff_line = line.split()
+    result = []
+    for i in range(0, len(buff_line), words):
+        word_line = buff_line[i: i + words]
+        if len(word_line[-1]) > words:
+            result.append(" ".join(word_line[:-1]))
+            result.append(word_line[-1])
+        else:
+            result.append(" ".join(word_line))
+    return result
+
+
 def print_solutions(kb: KnowledgeBase, solutions):
     """Процедура вывода ответов в читаемой форме"""
+    tabs = '\t'
     print("\nВозможные решения:")
     solutions_verbal = kb.get_verbal('solutions', solutions)
     if not solutions:
-        print(f"\tНет данных.")
+        print(f"{tabs}Нет данных.")
     else:
-        print(f"\tНомер\tНаименование\tИнформация")
         for row in solutions_verbal:
-            print(f"\t{row[0]}\t\t{row[1]}\t\t{row[2]}")
+            information = f"\n{tabs}\t".join(line_format(row[2], 15))
+            print(f"\n{tabs}Номер:\t{row[0]}\n"
+                  f"{tabs}Проблема:\t{row[1]}\n"
+                  f"{tabs}Решение:\n{tabs}\t{information}")
 
 
 def print_voting(task: Task, solutions):
@@ -30,7 +46,7 @@ def print_voting(task: Task, solutions):
 
 # Точка входа основной программы
 if __name__ == '__main__':
-    data_base = DataBase("KnowledgeBase.db")
+    data_base = DataBase("KnowledgeBase_rev.db")
     knowledge_base = KnowledgeBase(data_base)
     testTask = Task(knowledge_base)
     while not testTask.find_solution_step():
