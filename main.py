6db74f830e8fa8f7811ccb16cf1638616385144f
KaiNeon <neon030800@gmail.com>
2022-06-16 09:02:30 +0500

    Переработал функцию вывода результатов, добавил данные в базу реальных данных

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+from textwrap import wrap
 from task_module import Task
 from kb_module import KnowledgeBase
 from db_module import DataBase
@@ -6,32 +7,31 @@ from db_module import DataBase
 logging.basicConfig(level=logging.WARNING)
 
 
-def line_format(line, words):
-    buff_line = line.split()
-    result = []
-    for i in range(0, len(buff_line), words):
-        word_line = buff_line[i: i + words]
-        if len(word_line[-1]) > words:
-            result.append(" ".join(word_line[:-1]))
-            result.append(word_line[-1])
-        else:
-            result.append(" ".join(word_line))
-    return result
+def justify(line, width):
+    gap_width, max_replace = divmod(width - len(line) + line.count(' '), line.count(' '))
+    return line.replace(' ', ' ' * gap_width).replace(' ' * gap_width, ' ' * (gap_width + 1), max_replace)
+
+
+def lines_formatter(text, width, tabs):
+    lines = wrap(text, width, break_long_words=False)
+    for i, line in enumerate(lines[:-1]):
+        if len(line) <= width and line.count(' '):
+            lines[i] = justify(line, width).rstrip()
+    return f"\n{tabs}".join(lines)
 
 
 def print_solutions(kb: KnowledgeBase, solutions):
     """Процедура вывода ответов в читаемой форме"""
-    tabs = '\t'
-    print("\nВозможные решения:")
+    print("\nВозможные решения:\n")
     solutions_verbal = kb.get_verbal('solutions', solutions)
     if not solutions:
-        print(f"{tabs}Нет данных.")
+        print(f"\tНет данных.")
     else:
         for row in solutions_verbal:
-            information = f"\n{tabs}\t".join(line_format(row[2], 15))
-            print(f"\n{tabs}Номер:\t{row[0]}\n"
-                  f"{tabs}Проблема:\t{row[1]}\n"
-                  f"{tabs}Решение:\n{tabs}\t{information}")
+            information = lines_formatter(row[2], 60, '\t\t')
+            print(f"\tНомер:\t{row[0]}\n"
+                  f"\tПроблема:\t{row[1]}\n"
+                  f"\tРешение:\n\t\t{information}\n")
 
 
 def print_voting(task: Task, solutions):
